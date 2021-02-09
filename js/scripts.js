34a39ca11b688e55853989e5fb032f9c8728e731
Mauricio Linares <43184012+degstort@users.noreply.github.com>
2021-02-08 20:20:40 -0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+const btnToggle = document.querySelector('.toggle');
+
+btnToggle.addEventListener('click', function(){
+     document.getElementById('menuToggle').classList.toggle('header__nav');
+});
